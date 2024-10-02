0cdc3404612f23bd7fbdd446e6d97f9816fff08b
Djuniorr <danieljuniorpf9@gmail.com>
2024-10-02 19:37:15 -0300

    att db

--- api/db.js
+++ api/db.js
@@ -4,12 +4,15 @@ export const db = mysql.createConnection({
     host: process.env.MYSQL_HOST || 'localhost', // Substitua por 'localhost' para desenvolvimento local
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "login"
+    database: "login",
+    port: process.env.MYSQLPORT
+
 })
 
 export const ubs_db = mysql.createConnection({
     host: process.env.MYSQL_HOST || 'localhost', // Substitua por 'localhost' para desenvolvimento local
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "ubs"
+    database: "ubs",
+    port: process.env.MYSQLPORT
 })
