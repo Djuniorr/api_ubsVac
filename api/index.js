4bdf623c814c91fa2e88a5bca01c2fa25abbe3e4
Djuniorr <danieljuniorpf9@gmail.com>
2024-10-02 19:39:49 -0300

    debug

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@ import express from "express";
 import userRoutes from "./routes/users.js"
 import cors from "cors";
 
+console.log('Host:', process.env.MYSQL_HOST);
+console.log('User:', process.env.MYSQL_USER);
+console.log('Password:', process.env.MYSQL_PASSWORD);
+console.log('Port:', process.env.MYSQL_PORT);
+
+
 const app = express();
 
 app.use(express.json());
