0414bd0743eda12ac3098135768693ba282861f5
Djuniorr <danieljuniorpf9@gmail.com>
2024-10-02 19:38:29 -0300

    att db

--- api/db.js
+++ api/db.js
@@ -1,18 +1,22 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'localhost', // Substitua por 'localhost' para desenvolvimento local
+    host: process.env.MYSQL_HOST, // Use a variável de ambiente do Railway
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: "login",
-    port: process.env.MYSQLPORT
-
-})
+    port: process.env.MYSQL_PORT || 3306, // Use a porta correta
+});
 
 export const ubs_db = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'localhost', // Substitua por 'localhost' para desenvolvimento local
+    host: process.env.MYSQL_HOST, // Use a variável de ambiente do Railway
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: "ubs",
-    port: process.env.MYSQLPORT
-})
+    port: process.env.MYSQL_PORT || 3306, // Use a porta correta
+});
+
+console.log('Host:', process.env.MYSQL_HOST);
+console.log('User:', process.env.MYSQL_USER);
+console.log('Password:', process.env.MYSQL_PASSWORD);
+console.log('Port:', process.env.MYSQL_PORT);
